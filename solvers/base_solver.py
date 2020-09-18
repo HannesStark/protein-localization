274e1b771d21e2cfcafa5181d57c1123a89493b4
HannesStaerk <hannes.staerk@gmail.com>
2020-09-18 16:51:52 +0200

    add confusion matrix

--- solvers/base_solver.py
+++ solvers/base_solver.py
@@ -11,15 +11,19 @@ class BaseSolver():
         self.args = args
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
-        self.writer = SummaryWriter('runs/{}_{}'.format(args.experiment_name, datetime.now().strftime('%d-%m_%H-%M')))
+        self.writer = SummaryWriter(
+            'runs/{}_{}'.format(args.experiment_name, datetime.now().strftime('%d-%m_%H-%M-%S')))
 
     def train(self, train_loader, val_loader):
         args = self.args
+        t_iters = len(train_loader)  # number of iterations in training loop
+        v_iters = (len(val_loader) or not len(val_loader))  # number of iterations in validation loop or 1 if it's empty
 
         for epoch in range(args.num_epochs):  # loop over the dataset multiple times
             self.model.train()
+            train_preds = []  # predictions during training
+            train_labels = []
             train_loss = 0
-            train_acc = 0  # training accuracy
             for i, batch in enumerate(train_loader):
                 embedding, label = batch
                 embedding, label = embedding.to(self.device), label.to(self.device)
@@ -31,20 +35,21 @@ class BaseSolver():
                 loss.backward()
                 self.optim.step()
 
-                prediction = torch.max(prediction, dim=1)[1]  # get indices of the highes value in the prediction tensor
-                acc = (prediction == label).sum().item() / args.batch_size
-                train_acc += acc
+                train_preds.append(prediction.detach().cpu())
+                train_labels.append(label.detach().cpu())
                 loss_item = loss.item()
                 train_loss += loss_item
-                if i % args.log_iterations == args.log_iterations - 1:
+                if i % args.log_iterations == args.log_iterations - 1:  # log every log_iterations
                     print('[Epoch %d, Iteration %5d/%5d] TRAIN loss: %.7f' % (
-                        epoch + 1, i + 1, len(train_loader), loss_item))
+                        epoch + 1, i + 1, t_iters, loss_item))
+                    prediction = torch.max(prediction, dim=1)[1]  # get indices of the highest value in the prediction
                     print('[Epoch %d, Iteration %5d/%5d] TRAIN accuracy: %.4f%%' % (
-                        epoch + 1, i + 1, len(train_loader), acc * 100))
+                        epoch + 1, i + 1, t_iters, 100 * (prediction == label).sum().item() / args.batch_size))
 
             self.model.eval()
+            val_preds = []  # predictions during validation
+            val_labels = []
             val_loss = 0
-            val_acc = 0  # validation accuracy
             for i, batch in enumerate(val_loader):
                 embedding, label = batch
                 embedding, label = embedding.to(self.device), label.to(self.device)
@@ -52,20 +57,30 @@ class BaseSolver():
                 prediction = self.model(embedding)
 
                 loss = self.loss_func(prediction, label)
-                prediction = torch.max(prediction, dim=1)[1]  # get indices of the highes value in the prediction tensor
-                val_acc += (prediction == label).sum().item() / args.batch_size
+                val_preds.append(prediction.detach().cpu())
+                val_labels.append(label.detach().cpu())
                 val_loss += loss.item()
 
-            print(
-                '[Epoch %d] VAL accuracy: %.4f%% train accuracy: %.4f%%' % (
-                    epoch + 1, 100 * val_acc / (len(val_loader) or not len(val_loader)),
-                    100 * train_acc / len(train_loader)))
-            self.writer.add_scalars('Epoch Accuracy', {'train acc': 100 * train_acc / len(train_loader),
-                                                       'val acc': 100 * val_acc / (
-                                                               len(val_loader) or not len(val_loader))},
-                                    epoch)
-            self.writer.add_scalars('Epoch Loss', {'train loss': train_loss / len(train_loader),
-                                                   'val loss': val_loss / (len(val_loader) or not len(val_loader))},
+            train_preds = torch.cat(train_preds)
+            train_labels = torch.cat(train_labels)
+            val_preds = torch.cat(val_preds)
+            val_labels = torch.cat(val_labels)
+
+            val_confusion = torch.zeros((val_preds.shape[-1], val_preds.shape[-1]), dtype=torch.int64)
+            train_confusion = torch.zeros((train_preds.shape[-1], train_preds.shape[-1]), dtype=torch.int64)
+            train_preds = torch.max(train_preds, dim=1)[1]  # get indices of the highest value in the prediction
+            val_preds = torch.max(val_preds, dim=1)[1]  # get indices of the highest value in the prediction
+            for pred_index, true_index in zip(train_preds, train_labels):  # + 1 for each predicted row and true column
+                train_confusion[pred_index, true_index] += 1
+            for pred_index, true_index in zip(val_preds, val_labels):
+                val_confusion[pred_index, true_index] += 1
+
+            train_acc = 100 * (train_preds == train_labels).sum().item() / len(train_preds)
+            val_acc = 100 * (val_preds == val_labels).sum().item() / len(val_preds)
+            print('[Epoch %d] VAL accuracy: %.4f%% train accuracy: %.4f%%' % (epoch + 1, val_acc, train_acc))
+            # write to tensorboard
+            self.writer.add_scalars('Epoch Accuracy', {'train acc': train_acc, 'val acc': val_acc}, epoch)
+            self.writer.add_scalars('Epoch Loss', {'train loss': train_loss / t_iters, 'val loss': val_loss / v_iters},
                                     epoch)
 
             # TODO: implement saving of model
