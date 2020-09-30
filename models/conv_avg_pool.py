e912c302f059406a339ec1333a1d643627e1b1f8
Hannes Stärk <hannes.staerk@gmail.com>
2020-09-30 11:33:41 +0200

    add server config

--- models/conv_avg_pool.py
+++ models/conv_avg_pool.py
@@ -4,22 +4,16 @@ import torch.nn.functional as F
 
 
 class ConvAvgPool(nn.Module):
-    def __init__(self, embeddings_dim: int = 1024, n_conv_layers: int = 5):
+    def __init__(self, embeddings_dim: int = 1024, dropout=0.25):
         super(ConvAvgPool, self).__init__()
 
-        self.conv1 = nn.Conv1d(1024, 256, 20, stride=3)
-        self.conv2 = nn.Conv1d(256, 128, 15, stride=2)
-        self.conv_layers = nn.ModuleList()
-        for i in range(n_conv_layers):
-            self.conv_layers.append(nn.Sequential(
-                nn.Conv1d(128, 128, 3, stride=1),
-                nn.Sigmoid(),
-                nn.BatchNorm1d(128)
-            ))
+        self.conv1 = nn.Conv1d(embeddings_dim, embeddings_dim, 21, stride=1)
+        self.conv2 = nn.Conv1d(embeddings_dim, 512, 21, stride=1)
+        self.conv3 = nn.Conv1d(512, 512, 3, stride=1)
 
         self.linear = nn.Sequential(
-            nn.Linear(256, 32),
-            nn.Dropout(0.25),
+            nn.Linear(512, 32),
+            nn.Dropout(dropout),
             nn.ReLU(),
             nn.BatchNorm1d(32)
         )
@@ -35,12 +29,8 @@ class ConvAvgPool(nn.Module):
             classification: [batch_size,output_dim] tensor with logits
         """
         o = F.relu(self.conv1(x))
-        # o = F.relu(self.conv2(x))
-        # o = torch.sigmoid(self.conv3(o))
-        # for conv_layer in self.conv_layers:
-        #    o = conv_layer(o)
+        o = F.relu(self.conv2(o))
+        o = F.relu(self.conv3(o))
         o = torch.mean(o, dim=-1)
-        # max_pool, _ = torch.max(o, dim=-1)
-        # o = torch.cat([avg_pool, max_pool], dim=-1)
         o = self.linear(o)
         return self.output(o)
