a27162148944b99443e69b665182da5468f5e3e6
Hannes Stärk <hannes.staerk@gmail.com>
2020-10-23 10:33:35 +0200

    very small fix

--- tmp_server.py
+++ tmp_server.py
@@ -4,6 +4,6 @@ for type in ['cat']:
 #for type in ['cat', 'sum', 'avg', 'max']:
     for stage in ['train', 'val', 'test']:
         combine_embeddings('/mnt/project/bio_embeddings/runs/hannes/embed_' + stage + '/embeddings/bert_embeddings/embeddings_file.h5',
-                           '/mnt/project/bio_embeddings/runs/hannes/embed_' + stage + '_seqvec/embeddings/bert_embeddings/embeddings_file_summed.h5',
+                           '/mnt/project/bio_embeddings/runs/hannes/embed_' + stage + '_seqvec/embeddings/bert_embeddings/embeddings_file.h5',
                            output_path='/mnt/project/bio_embeddings/runs/hannes/combined_embeddings/' + type + '_' + stage + '.h5',
                            type=type)
