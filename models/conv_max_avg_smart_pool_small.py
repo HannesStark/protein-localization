attention with correct masking in first_attention.py

models/conv_max_avg_smart_pool_small.py
@@ -22,10 +22,12 @@ class ConvMaxAvgSmartPoolSmall(nn.Module):
         )
         self.output = nn.Linear(32, output_dim)
 
-    def forward(self, x: torch.Tensor, seq_len) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask) -> torch.Tensor:
         """
         Args:
             x: [batch_size, embeddings_dim, sequence_length] embedding tensor that should be classified
+            mask: [batch_size, sequence_length] mask corresponding to the zero padding used for the shorter sequecnes in the batch. All values corresponding to padding are False and the rest is True.
+
 
         Returns:
             classification: [batch_size,output_dim] tensor with logits

models/first_attention.py
@@ -4,11 +4,13 @@ import torch.nn.functional as F
 
 
 class FirstAttention(nn.Module):
-    def __init__(self, embeddings_dim=1024, output_dim=11, dropout=0.25, kernel_size = 7):
+    def __init__(self, embeddings_dim=1024, output_dim=11, dropout=0.25, kernel_size=7, conv_dropout: float = 0.25):
         super(FirstAttention, self).__init__()
 
-        self.conv1 = nn.Conv1d(embeddings_dim, embeddings_dim, kernel_size, stride=1, padding=kernel_size//2)
+        self.conv1 = nn.Conv1d(embeddings_dim, embeddings_dim, kernel_size, stride=1, padding=kernel_size // 2)
         self.attend = nn.Conv1d(embeddings_dim, embeddings_dim, 1, stride=1)
+
+        self.dropout = nn.Dropout(conv_dropout)
 
         self.linear = nn.Sequential(
             nn.Linear(embeddings_dim, 32),
@@ -19,16 +21,19 @@ class FirstAttention(nn.Module):
 
         self.output = nn.Linear(32, output_dim)
 
-    def forward(self, x: torch.Tensor, seq_len) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask) -> torch.Tensor:
         """
         Args:
             x: [batch_size, embeddings_dim, sequence_length] embedding tensor that should be classified
+            mask: [batch_size, sequence_length] mask corresponding to the zero padding used for the shorter sequecnes in the batch. All values corresponding to padding are False and the rest is True.
 
         Returns:
             classification: [batch_size,output_dim] tensor with logits
         """
         o = self.conv1(x)
-        attention = self.attend(o)
+        o = self.dropout(o)
+        attention = self.attend(x)
+        attention = attention.masked_fill(mask[:, None, :] == False, float('-inf'))
         o = torch.sum(o * F.softmax(attention, dim=-1), dim=-1)  # [batchsize, embeddingsdim]
         o = self.linear(o)
         return self.output(o)

models/legacy/conv_max_avg_pool_sigmoid.py
@@ -18,7 +18,7 @@ class ConvMaxAvgPoolSigmoid(nn.Module):
         )
         self.output = nn.Linear(32, output_dim)
 
-    def forward(self, x: torch.Tensor, seq_len) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask) -> torch.Tensor:
         """
         Args:
             x: [batch_size, embeddings_dim, sequence_length] embedding tensor that should be classified

models/lstm_conv.py
@@ -22,7 +22,7 @@ class LSTMConv(nn.Module):
         )
         self.output = nn.Linear(32, output_dim)
 
-    def forward(self, x, seq_len):
+    def forward(self, x, mask):
         """
         Args:
             x: [batch_size, embeddings_dim, sequence_length] embedding tensor that should be classified

utils/explore_embeddings.py
@@ -38,7 +38,7 @@ def explore_embeddings(args):
     model.conv1.register_forward_hook(visualize_activation_hook)
     model(embedding, metadata['length'])
 
-    plt.plot(torch.max(embedding[1],dim=0)[0])
+    plt.plot(torch.max(embedding[1], dim=0)[0])
     plt.title('embedding max')
     plt.show()
 
@@ -56,7 +56,6 @@ def visualize_activation_hook(self, input, output):
     inp_max = torch.max(inp, dim=0)[0]
     out_avg = torch.mean(out, dim=0)
     out_max = torch.max(out, dim=0)[0]
-    print(out_max[-1])
     plt.plot(inp_max)
     plt.title('input max')
     plt.show()