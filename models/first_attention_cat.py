2c49e67f7dde3e4c5620c1f19d7adb2efc9310dc
Hannes Stärk <hannes.staerk@gmail.com>
2020-10-30 09:03:55 +0100

    add relu to first attention

--- models/first_attention_cat.py
+++ models/first_attention_cat.py
@@ -30,8 +30,9 @@ class FirstAttention(nn.Module):
         Returns:
             classification: [batch_size,output_dim] tensor with logits
         """
+        embeddings_dim = x.shape[1]
         mask = mask[:, None, :]  # add singleton dimension for broadcasting
-        o = F.relu(self.conv1(x))
+        o = self.conv1(x)
         o = self.dropout(o)
         attention = self.attend(x)
         attention = attention.masked_fill(mask == False, -1e9)
@@ -39,4 +40,22 @@ class FirstAttention(nn.Module):
         o2, _ = torch.max(o, dim=-1)
         o = torch.cat([o1, o2], dim=-1)
         o = self.linear(o)
+        return self.output(o)
+
+        mask = mask[:, None, :]  # add singleton dimension for broadcasting
+        o1 = F.relu(self.conv1(x[:, :embeddings_dim // 2, :]))
+        o1 = self.dropout1(o1)
+        o1_avg = torch.sum(o1 * mask, dim=-1) / mask.sum(dim=-1)
+        o1_max, _ = torch.max(o1, dim=-1)
+        o1 = torch.cat([o1_avg, o1_max], dim=-1)
+        o1 = self.linear1(o1)
+
+        o2 = F.relu(self.conv2(x[:, embeddings_dim // 2:, :]))
+        o2 = self.dropout2(o2)
+        o2_avg = torch.sum(o2 * mask, dim=-1) / mask.sum(dim=-1)
+        o2_max, _ = torch.max(o2, dim=-1)
+        o2 = torch.cat([o2_avg, o2_max], dim=-1)
+        o2 = self.linear2(o2)
+
+        o = torch.cat([o1, o2], dim=-1)
         return self.output(o)
