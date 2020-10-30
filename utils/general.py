b9f59db27ffdd6ef56a3c638d1e9e374ff5eb8a1
Hannes Stärk <hannes.staerk@gmail.com>
2020-10-30 10:35:26 +0100

    test

--- utils/general.py
+++ utils/general.py
@@ -21,6 +21,8 @@ LOCALIZATION_abbrev = ['Mem', 'Cyt', 'End', 'Gol', 'Lys', 'Mit', 'Nuc', 'Per', '
 
 SOLUBILITY = ['M', 'S', 'U']
 
+def get_confusion_matrix(train_results: np.ndarray)-> np.ndarray:
+    pass
 
 def tensorboard_confusion_matrix(train_results: np.ndarray, val_results: np.ndarray, writer: SummaryWriter, args,
                                  step: int):
@@ -45,6 +47,7 @@ def tensorboard_confusion_matrix(train_results: np.ndarray, val_results: np.ndar
         val_confusion = confusion_matrix(val_results[:, 1], val_results[:, 0])  # confusion matrix for validation
         train_cm = pd.DataFrame(train_confusion, LOCALIZATION_abbrev, LOCALIZATION_abbrev)
         val_cm = pd.DataFrame(val_confusion, LOCALIZATION_abbrev, LOCALIZATION_abbrev)
+        print(np.diag(val_cm)/np.array(val_cm).sum(1))
 
     fig, ax = plt.subplots(1, 2, figsize=(15, 6.5))
     ax[0].set_title('Training')
