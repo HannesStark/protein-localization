5187fd856838d2fba213f8f3ac85137f1ad8a91b
Hannes Stärk <hannes.staerk@gmail.com>
2020-11-01 09:49:34 +0100

    explore embeddings and move softmax to module

--- utils/explore_embeddings.py
+++ utils/explore_embeddings.py
@@ -32,22 +32,23 @@ def explore_embeddings(args):
     # Needs "from models import *" to work
     model = globals()[args.model_type](embeddings_dim=dataset[0][0].shape[-1], **args.model_parameters)
 
-    embedding, localization, solubility, metadata = next(iter(data_loader))
+    with torch.no_grad():
+        embedding, localization, solubility, metadata = next(iter(data_loader))
 
-    # register defined hooks and run trough some example in the dataset
-    # model.conv1.register_forward_hook(visualize_activation_hook)
-    model.attend.register_forward_hook(visualize_activation_hook)
+        # register defined hooks and run trough some example in the dataset
+        # model.conv1.register_forward_hook(visualize_activation_hook)
+        model.softmax.register_forward_hook(visualize_activation_hook)
 
-    # create mask corresponding to the zero padding used for the shorter sequecnes in the batch. All values corresponding to padding are False and the rest is True.
-    mask = torch.arange(metadata['length'].max())[None, :] < metadata['length'][:, None]  # [batchsize, seq_len]
-    model(embedding, mask)
+        # create mask corresponding to the zero padding used for the shorter sequecnes in the batch. All values corresponding to padding are False and the rest is True.
+        mask = torch.arange(metadata['length'].max())[None, :] < metadata['length'][:, None]  # [batchsize, seq_len]
+        model(embedding, mask)
 
-    plt.plot(torch.max(embedding[1], dim=0)[0])
-    plt.title('embedding max')
-    plt.show()
+        plt.plot(torch.max(embedding[1], dim=0)[0])
+        plt.title('embedding max')
+        plt.show()
 
 
-def visualize_activation_hook(self, input, output):
+def visualize_activation_hook(self, input, output, clamp=False):
     print('Inside ' + self.__class__.__name__ + ' forward')
     print('')
     print('input size:', input[0].size())
@@ -59,6 +60,8 @@ def visualize_activation_hook(self, input, output):
     out = output.data[1].squeeze()
     inp_max = torch.max(inp, dim=0)[0]
     out_avg = torch.mean(out, dim=0)
+    if clamp:
+        out_avg = out_avg.clamp(out_avg.max() - 0.00005, out_avg.max())
     out_max = torch.max(out, dim=0)[0]
     plt.plot(inp_max)
     plt.title('input max')
