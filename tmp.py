330104ddf1635712e50128f1fcd562b3b280b85e
Hannes Stärk <hannes.staerk@gmail.com>
2020-12-16 10:33:32 +0100

    new plots

--- tmp.py
+++ tmp.py
@@ -2,8 +2,8 @@ import os
 
 
 import numpy as np
-#
-#res = np.array([80.56, 80.39, 79.91, 80.28, 80.33])
-#
-#print(res.mean())
-#print(res.std())
+
+res = np.array([80.56, 80.39, 79.91, 80.28, 80.33])
+
+print(res.mean())
+print(res.std())
