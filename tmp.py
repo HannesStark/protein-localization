34e84d11208f7ea93f69a2607a8593ee997dc70f
Hannes Stärk <hannes.staerk@gmail.com>
2020-12-21 08:24:55 +0100

    ignore single missing sequecne

--- tmp.py
+++ tmp.py
@@ -20,9 +20,11 @@ appendix = ['hannes_deeploc_t5-encoderOnly.h5',
 save_appendix = ['t5-encoderOnly.h5',
                  't5-encoderOnlyHALF.h5']
 
-fasta_paths = ['data/fasta_files/train.fasta',
-               'data/fasta_files/val_homreduced.fasta',
-               'data/fasta_files/test_as_per_deeploc.fasta']
+fasta_paths = [
+    'data/fasta_files/train.fasta',
+    'data/fasta_files/val_homreduced.fasta',
+    'data/fasta_files/test_as_per_deeploc.fasta'
+]
 
 save_name = ['train_',
              'val_',
@@ -33,8 +35,14 @@ save_paths = []
 
 for i, append in enumerate(appendix):
     embeddings_file = h5py.File(os.path.join(base_path, append), 'r')
+    print(len(embeddings_file.keys()))
     for split_index, fasta_path in enumerate(fasta_paths):
+        print(fasta_path)
         save_file = h5py.File(os.path.join('data/embeddings', save_name[split_index] + save_appendix[i]), 'w')
         for record in SeqIO.parse(open(fasta_path), 'fasta'):
-            save_file.create_dataset(record.description,
-                                     data=embeddings_file[str(record.description).replace('.', '_').replace('/', '_')])
+            try:
+                save_file.create_dataset(record.description,
+                                         data=embeddings_file[
+                                             str(record.description).replace('.', '_').replace('/', '_')])
+            except:
+                print(str(record.description).replace('.', '_').replace('/', '_'))
