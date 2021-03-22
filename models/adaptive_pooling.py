b685b59a3c470262e811a08d72e8367b1f5303dd
Hannes Stärk <hannes.staerk@gmail.com>
2021-03-22 08:03:08 +0100

    add adaptive pooling experiment

--- models/adaptive_pooling.py
+++ models/adaptive_pooling.py
@@ -7,17 +7,16 @@ class AdaptivePooling(nn.Module):
     def __init__(self, embeddings_dim=1024, output_dim=11, dropout=0.25, kernel_size=9, conv_dropout: float = 0.25):
         super(AdaptivePooling, self).__init__()
 
-        self.conv1 = nn.Conv1d(embeddings_dim, embeddings_dim, 3, stride=1)
-        self.conv2 = nn.Conv1d(embeddings_dim, embeddings_dim, 15, stride=1)
-        self.conv3 = nn.Conv1d(embeddings_dim, embeddings_dim, 9, stride=1)
+        self.conv1 = nn.Conv1d(embeddings_dim, embeddings_dim, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv1d(embeddings_dim, embeddings_dim, 15, stride=1, padding=7)
+        self.conv3 = nn.Conv1d(embeddings_dim, embeddings_dim, 9, stride=1, padding=4)
 
         self.adaptive_pool = nn.AdaptiveAvgPool1d(5)
-
 
         self.dropout = nn.Dropout(conv_dropout)
 
         self.linear = nn.Sequential(
-            nn.Linear(3 * embeddings_dim, 32),
+            nn.Linear(5 * 3 * embeddings_dim, 32),
             nn.Dropout(dropout),
             nn.ReLU(),
             nn.BatchNorm1d(32)
@@ -43,9 +42,8 @@ class AdaptivePooling(nn.Module):
         o2 = F.relu(self.conv2(x))
         o3 = F.relu(self.conv3(x))
 
-
-        o = torch.cat([o1, o2, o3], dim=-1)
+        o = torch.cat([o1, o2, o3], dim=1)
         o = self.adaptive_pool(o)
-
+        o = o.view(o.shape[0], -1)
         o = self.linear(o)  # [batchsize, 32]
         return self.output(o)  # [batchsize, output_dim]
