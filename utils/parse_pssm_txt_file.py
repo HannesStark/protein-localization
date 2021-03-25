686ad8bd2e69c4bef4bc1850f95cbf6b287d1e31
Hannes Stärk <hannes.staerk@gmail.com>
2021-03-25 21:13:07 +0100

    train

--- utils/parse_pssm_txt_file.py
+++ utils/parse_pssm_txt_file.py
@@ -1,4 +1,5 @@
 import difflib
+from collections import defaultdict
 
 import h5py
 import pandas as pd
@@ -20,31 +21,32 @@ count = 0
 # Strips the newline character
 skipnext = False
 with h5py.File('../data/embeddings/subCell_all_pssmFile.h5', 'w') as hf:
-    sequences = []
+    pssms = {}
+    lengths = defaultdict(list)
     sequence = []
     pssm = []
     for i, line in tqdm(enumerate(Lines[2:])):
         if not skipnext:
             string = line.strip().split()
-
             if string[0] == 'Query':
-                print(i)
                 skipnext = True
                 pssm_sequence = ''.join(sequence)
-                if pssm_sequence not in sequences:
-                    sequence_id = difflib.get_close_matches(pssm_sequence, fasta_sequences, n=1, cutoff=0)
-                    print('sequence_id', sequence_id[0])
-                    hf.create_dataset(sequence_id[0], data=torch.stack(pssm))
-                    sequences.append(pssm_sequence)
+                pssms[pssm_sequence] = torch.stack(pssm)
+                lengths[str(len(pssm_sequence))].append(pssm_sequence)
                 sequence = []
                 pssm = []
             else:
-                if string[1] != 'X':
-                    sequence.append(string[1])
+
+                sequence.append(string[1])
                 pssm.append(torch.tensor(np.array(string[2:], dtype=int)))
         else:
             skipnext = False
-    hf.create_dataset(''.join(sequence), data=torch.stack(pssm))
+
+    for i, fasta_sequence in tqdm(enumerate(list(set(fasta_sequences)))):
+        lookup_sequences = lengths[str(len(fasta_sequence))]
+        sequence_id = difflib.get_close_matches(fasta_sequence, lookup_sequences, n=1, cutoff=0)[0]
+        pssm = pssms[sequence_id]
+        hf.create_dataset(fasta_sequence, data=pssms[sequence_id])
 
 with h5py.File('../data/embeddings/subCell_all_pssmFile.h5', 'r') as hf:
     keys = list(hf.keys())
